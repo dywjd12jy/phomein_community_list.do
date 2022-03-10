4020a8f11a0813a2a27ad7600230224947e0fdb1
dywjd12jy <dywjd3045@gmail.com>
2022-03-11 01:57:36 +0900

    js error

--- js/jquery.js
+++ js/jquery.js
@@ -1,12 +1,40 @@
-$(function(){
-    //1019이상 nav 호버시 슬라이드 이벤트
-    var Header_DT = $('#desktop.header')
-    var Nav = $('#desktop nav')
+// $(function(){
+//     //1019이상 nav 호버시 슬라이드 이벤트
+//     var Header_DT = $('#desktop.header')
+//     var Nav = $('#desktop nav')
     
-    Nav.on("mouseover",function(){
-        Header_DT.addClass('active');
-    }); 
-    Nav.on("mouseout",function(){
-        Header_DT.removeClass('active');
-    });
-});
+//     Nav.on("mouseover",function(){
+//         Header_DT.addClass('active');
+//     }); 
+//     Nav.on("mouseout",function(){
+//         Header_DT.removeClass('active');
+//     });
+// });
+
+var location = document.querySelector('.location_sub_menu');
+var active = document.querySelector('.active');
+
+// function myFunc() {
+//     var para = document.getElementById("p");
+//     para.classList.toggle("paragraphClass");
+// }
+location.addEventListener('mouseenter', function(){
+    this.classList.remove('active');
+    this.classList.add('active');
+})
+// container.addEventListener('mouseleave', function(){
+//     this.classList.add('first');
+//     this.classList.remove('second');
+// })
+//     location.addEventListener('click', function() {
+//         // console.log("active됌");
+//     if(location.classList.contains(active)){
+//         location.classList.remove(active);
+//     } else {
+//         location.classList.add(active);
+//     }
+// });
+// (function(){} location.addEventListener('click', function() {
+//     console.log("active됌");
+// //     location.classList.toggle('active');
+// });
--- js/js.js
+++ js/js.js
@@ -10,3 +10,19 @@
 //         clicked = 1;
 //     }
 // }
+// var location = document.querySelector('.location_sub_menu');
+// var Active = document.querySelector('.active');
+
+//     location.addEventListener('click', function() {
+//         console.log("active됌");
+//     if(location.classList.contains(Active)){
+//         location.classList.remove(Active);
+//     } else {
+//         location.classList.add(Active);
+//     }
+// });
+
+// location.addEventListener('click', function() {
+//     console.log("active됌");
+//     location.classList.toggle('active');
+// });
