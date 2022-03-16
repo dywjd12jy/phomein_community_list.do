d0e09337d381dd8f8a5f11c6849367e24d0f0404
dywjd12jy <dywjd3045@gmail.com>
2022-03-16 21:44:05 +0900

    css import

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    document.getElementById("header").innerHTML='<object type="text/html" data="header.html"></object>';
+})
+
+$(document).ready(function(){
+    document.getElementById("footer").innerHTML='<object type="text/html" data="https://dywjd12jy.github.io/phomein_community_list.do/footer.html"></object>';
+})
